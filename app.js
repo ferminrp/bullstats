5e0486d606a5fa45b4aa26ada0a0918dd958a87b
ferminrp <frodriguezpenelas@gmail.com>
2021-04-21 12:34:07 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const teamRoutes = require('./routes/teamRoutes');
+const playerRoutes = require('./routes/playerRoutes');
+const indexRoutes = require('./routes/indexRoutes');
+
+const port = process.env.PORT;
+
+const methodOverride = require('method-override')   
+// Requerimos este módulo para asegurar compatibilidad de métodos PUT y DELETE en todos los navegadores.
+app.use(methodOverride('_method'))
+// Para configurarlo indicamos a app que use este método, con app.use lo empleamos a nivel aplicación.
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// Rutas
+app.set('view engine', 'ejs');
+
+app.use('/', indexRoutes);
+app.use('/players', playerRoutes);
+app.use('/teams', teamRoutes);
+
+
+app.listen(port || 3000, () => console.log('Servidor escuchando en puerto 3000'));
---
+++ controllers/homeController.js
@@ -0,0 +1,5 @@
+module.exports = {
+    home: (req, res) => {
+        res.send('Esta es la home!')
+    }
+};
