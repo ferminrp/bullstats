77aa373d797e8e5cc09a698f7184c87fcb5e9e1f
ferminrp <frodriguezpenelas@gmail.com>
2021-05-05 14:11:33 -0300

    en nightly reemplazo los var por let

--- public/scripts/league.js
+++ public/scripts/league.js
@@ -1,182 +1,182 @@
-const pathname = window.location.pathname.split("/");
-const league = pathname[pathname.length - 1];
-console.log(league);
+const pathname = window.location.pathname.split("/")
+const league = pathname[pathname.length - 1]
+console.log(league)
 
-var url;
-var division;
+let url;
+let division;
 
 switch (league) {
-	case "liga-argentina": {
-		var url = "https://bullstats-default-rtdb.firebaseio.com/tables/LARG.json";
-		var division = "LEAGUE";
-		break;
-	}
-	case "liga-argentina-conferencia-norte": {
-		var url = "https://bullstats-default-rtdb.firebaseio.com/tables/LARG.json";
-		var division = "NORTE";
-		break;
-	}
-	case "liga-argentina-conferencia-sur": {
-		var url = "https://bullstats-default-rtdb.firebaseio.com/tables/LARG.json";
-		var division = "SUR";
-		break;
-	}
+    case 'liga-argentina':
+        let url = "https://bullstats-default-rtdb.firebaseio.com/tables/LARG.json";
+        let division = "LEAGUE"
+        break;
+    case 'liga-argentina-conferencia-norte':
+        let url = "https://bullstats-default-rtdb.firebaseio.com/tables/LARG.json";
+        let division = "NORTE"
+        break;
+    case 'liga-argentina-conferencia-sur':
+        let url = "https://bullstats-default-rtdb.firebaseio.com/tables/LARG.json";
+        let division = "SUR"
+        break;
 }
 
 function sectionBuilder(data) {
-	console.log(data);
-	// defino el main y creo dentro un section
-	var main = document.getElementById("app");
-	var section = document.createElement("section");
-	// Llamo a la funcion que crea headers y mando a hacer uno y lo appendeo al section
-	section.appendChild(headerBuilder(data.name));
-	// creo la tabla dentro de un componente responsive
+    console.log(data);
+    // defino el main y creo dentro un section
+    let main = document.getElementById('app');
+    let section = document.createElement('section');
+    // Llamo a la funcion que crea headers y mando a hacer uno y lo appendeo al section
+    section.appendChild(headerBuilder(data.name));
+    // creo la tabla dentro de un componente responsive
 
-	var responsiveDiv = document.createElement("div");
-	responsiveDiv.style["overflow-x"] = "auto";
-	section.appendChild(responsiveDiv);
-	var table = document.createElement("table");
-	responsiveDiv.appendChild(table);
+    let responsive_div = document.createElement('div');
+    responsive_div.style["overflow-x"] = "auto";
+    section.appendChild(responsive_div);
+    let table = document.createElement('table');
+    responsive_div.appendChild(table);
 
-	// creo la row de columnas en una funcion aparte y la appendeo a la tabla
+    // creo la row de columnas en una funcion aparte y la appendeo a la tabla
+    
+    table.appendChild(columnsBuilder(data.data.columns));
 
-	table.appendChild(columnsBuilder(data.data.columns));
+    // Creo el resto de las rows
 
-	// Creo el resto de las rows
+    for(row in data.data.data) {
+        let row = data.data.data[row];
+        rowBuilder(row);
+        table.appendChild(rowBuilder(row));
+    }
 
-	for (row in data.data.data) {
-		var row = data.data.data[row];
-		rowBuilder(row);
-		table.appendChild(rowBuilder(row));
-	}
-
-	// meto el section que estuve creando dentro del main
-	main.appendChild(section);
+    // meto el section que estuve creando dentro del main
+    main.appendChild(section);
 }
 
 function columnsBuilder(data) {
-	var columnsRow = document.createElement("tr");
-	for (column in data) {
-		var columnData = data[column];
-		var node = document.createElement("th");
-		var nodeText = document.createTextNode(columnData);
-		node.appendChild(nodeText);
-		columnsRow.appendChild(node);
-	}
-	return columnsRow;
+    let columns_row = document.createElement('tr');
+    for (column in data) {
+        let column_data = data[column];
+        let node = document.createElement('th');
+        let node_text = document.createTextNode(column_data)
+        node.appendChild(node_text);
+        columns_row.appendChild(node);
+    }
+    return columns_row
 }
 
 function rowBuilder(row) {
-	var rowNode = document.createElement("tr");
-	for (concept in row) {
-		var cell = document.createElement("td");
-		var cellText = document.createTextNode(row[concept]);
-		cell.appendChild(cellText);
-		rowNode.appendChild(cell);
-	}
-	return rowNode;
+    let row_node = document.createElement('tr');
+    for (concept in row) {
+        let cell = document.createElement('td');
+        let cell_text = document.createTextNode(row[concept])
+        cell.appendChild(cell_text);
+        row_node.appendChild(cell);
+    }
+    return row_node
 }
 
 function headerBuilder(name) {
-	var headingNode = document.createElement("h3");
-	var headingText = document.createTextNode(name);
-	headingNode.appendChild(headingText);
-	return headingNode;
+    let headingNode = document.createElement('h3');
+    let heading_text = document.createTextNode(name)
+    headingNode.appendChild(heading_text);
+    return headingNode
 }
 
 function teamSection(data) {
-	var main = document.getElementById("app");
-	var section = document.createElement("section");
-	section.appendChild(headerBuilder("Teams"));
 
-	var teamsGrid = document.createElement("div");
-	teamsGrid.className = "teams-grid";
+    let main = document.getElementById('app');
+    let section = document.createElement('section');
+    section.appendChild(headerBuilder("Teams"));
 
-	section.appendChild(teamsGrid);
+    let teams_grid = document.createElement('div');
+    teams_grid.className = "teams-grid";
 
-	var equipos = data[0][division].data.data;
+    section.appendChild(teams_grid);
 
-	for (row in equipos) {
-		var equipo = equipos[row];
-		teamsGrid.appendChild(equipoCardBuilder(equipo));
-	}
+    let equipos = data[0][division].data.data;
 
-	main.appendChild(section);
+    for (row in equipos) {
+        let equipo = equipos[row];
+        teams_grid.appendChild(equipoCardBuilder(equipo));
+    }
+
+    main.appendChild(section);
 }
 
 function equipoCardBuilder(equipoData) {
-	var equipoName = equipoData[1];
-	var equipoAbbreviation = equipoData[2];
-	console.log(equipoName);
+    let equipo_name = equipoData[1];
+    let equipo_abbreviation = equipoData[2];
+    console.log(equipo_name);
 
-	// Creo la card container de el logo
-	var equipoLink = document.createElement("a");
-	equipoLink.className = "equipo-link";
-	equipoLink.href = `/teams/${equipoAbbreviation}`;
+    // Creo la card container de el logo
+    let equipo_link = document.createElement('a');
+    equipo_link.className = "equipo-link"
+    equipo_link.href = "/teams/"+equipo_abbreviation
 
-	var equipoCard = document.createElement("div");
-	equipoCard.className = "equipo-card";
+    let equipo_card = document.createElement('div');
+    equipo_card.className  = "equipo-card";
 
-	// le meto la imagen a la card
-	var equipoImg = document.createElement("img");
-	equipoImg.src = `/images/logos/equipos/${equipoAbbreviation}.png`;
-	equipoImg.className = "equipo-img";
-	equipoImg.loading = "lazy";
-	equipoCard.appendChild(equipoImg);
+    // le meto la imagen a la card
+    let equipo_img = document.createElement('img');
+    equipo_img.src = "/images/logos/equipos/" + equipo_abbreviation + ".png"
+    equipo_img.className = "equipo-img"
+    equipo_img.loading = "lazy"
+    equipo_card.appendChild(equipo_img);
 
-	var equipoNameNode = document.createElement("p");
-	equipoNameNode.appendChild(document.createTextNode(equipoName));
-	equipoNameNode.className = "equipo-name";
-	equipoCard.appendChild(equipoNameNode);
+    let equipo_name_node = document.createElement('p');
+    equipo_name_node.appendChild(document.createTextNode(equipo_name));
+    equipo_name_node.className = "equipo-name";
+    equipo_card.appendChild(equipo_name_node);
 
-	equipoLink.appendChild(equipoCard);
+    equipo_link.appendChild(equipo_card);
 
-	return equipoLink;
+    return equipo_link;
 }
 
-fetch(url).then((resp) => resp.json()).then(function(data) {
-	// En base a todas las secciones que hay en el json voy creando secciones de heading + tabla
-	for (data_section in data) {
-		if (typeof data[data_section][division] !== "undefined") {
-			sectionBuilder(data[data_section][division]);
-		}
-	}
-	// Creo una seccion de teams enviandole los equipos que hay en esta pagina
-	teamSection(data);
-}).catch(function(error) {
-	console.log(error);
+fetch(url).then((resp) => resp.json()).then(function (data) {
+
+    // En base a todas las secciones que hay en el json voy creando secciones de heading + tabla
+    for(data_section in data) {
+        if (typeof data[data_section][division] !== "undefined") {
+            sectionBuilder(data[data_section][division]);
+        }
+    };
+
+    // Creo una seccion de teams enviandole los equipos que hay en esta pagina
+    teamSection(data);
+
+
+}).catch(function (error) {
+    console.log(error);
 });
 
 switch (division) {
-	case "NORTE": {
-		var elements = document.getElementsByClassName("conference-card");
-		var requiredElement = elements[0];
-		requiredElement.style.opacity = "50%";
+    case 'NORTE':
+        let elements = document.getElementsByClassName('conference-card');
+        let requiredElement = elements[0];
+        requiredElement.style.opacity = "50%";
 
-		var links = document.getElementsByClassName("conference-link");
 
-		links[0].href = "/leagues/liga-argentina-conferencia-sur";
-		links[1].href = "/leagues/liga-argentina";
+        let links = document.getElementsByClassName('conference-link');
 
-		break;
-	}
+        links[0].href = "/leagues/liga-argentina-conferencia-sur";
+        links[1].href = "/leagues/liga-argentina"
 
-	case "SUR": {
-		var elements = document.getElementsByClassName("conference-card");
-		var requiredElement = elements[1];
-		requiredElement.style.opacity = "50%";
+        break;
 
-		var links = document.getElementsByClassName("conference-link");
+    case 'SUR':
+        let elements = document.getElementsByClassName('conference-card');
+        let requiredElement = elements[1];
+        requiredElement.style.opacity = "50%";
 
-		links[0].href = "/leagues/liga-argentina";
-		links[1].href = "/leagues/liga-argentina-conferencia-norte";
+        let links = document.getElementsByClassName('conference-link');
 
-		break;
-	}
+        links[0].href = "/leagues/liga-argentina";
+        links[1].href = "/leagues/liga-argentina-conferencia-norte"
 
-	default: {
-		var elements = document.getElementsByClassName("conference-card");
-		elements[0].style.opacity = "50%";
-		elements[1].style.opacity = "50%";
-	}
+        break;
+
+    default:
+        let elements = document.getElementsByClassName('conference-card');
+        elements[0].style.opacity = "50%";
+        elements[1].style.opacity = "50%";
 }
