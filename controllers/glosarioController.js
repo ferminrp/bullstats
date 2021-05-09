d2265adc2e43c9b9f4853a1c6dd171ddfed8b8c7
ferminrp <frodriguezpenelas@gmail.com>
2021-05-09 20:55:56 -0300

    arreglo los links en glosario

--- controllers/glosarioController.js
+++ controllers/glosarioController.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 
 /* Instrucciones https://dev.to/khalby786/creating-a-markdown-blog-with-ejs-express-j40 */
 
-module.exports = {
+let glosarioController = {
 	definicion: (req, res) => {
 		    // read the markdown file
             const file = matter.read('./glosario/' + req.params.article + '.md');
@@ -23,11 +23,34 @@ module.exports = {
             });
 	},
     glosario: (req, res) => {
-
+            /* Creo un array de todos los posteos */
             const posts = fs.readdirSync('./glosario').filter(file => file.endsWith('.md'));
+
+            /* Creo un array donde voy a guardar los titulos de los posteos */
+            let titles = []
+
+            /*Por cada posteo tomo el titulo y lo guardo en el array de titulos */
+            for (post in posts) {
+              titles.push(glosarioController.getPostTitle( posts[post] ))
+            }
+
+            /* Creo un objeto juntando posts y titles */
+
+            let combinado = {};
+            posts.forEach((key, i) => combinado[key] = titles[i]);
+
+            console.log(combinado);
+
             res.render("glosario", {
-              posts: posts
+              posts: posts,
+              titles: titles
             });
 
-    }
+    },
+  getPostTitle: (postFileName) => {
+    const file = matter.read('./glosario/' + postFileName);
+    return file.data.title
+  }
 };
+
+module.exports = glosarioController;
